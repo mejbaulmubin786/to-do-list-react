2fecdfe25981e4079c30e2faf69d6783d4e9f982
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-10-17 02:14:38 +0600

    some functionalaty added

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,22 @@
 import { useEffect, useRef, useState } from 'react'
 import './css/Todo.css'
+import TodoItems from './TodoItems';
+
+let count = 0;
 
 const Todo = () => {
     const [todos, setTodos] = useState([]);
     const inputRef = useRef(null);
 
     const add = () =>{
-        console.log("ADD");
+        setTodos([...todos, {no:count, text:inputRef.current.value, display:""}]);
+        inputRef.current.value = "";
     }
+
+    useEffect(()=>{
+        console.log(todos);
+    }, [todos])
+
   return (
     <div className='todo'>
         <div className="todo-header">To-Do List</div>
@@ -15,7 +24,12 @@ const Todo = () => {
             <input ref={inputRef} type="text" placeholder='Add Your Task' className='todo-input' />
             <div onClick={()=>{add()}} className="todo-add-btn">Add</div>
         </div>
-        <div className="todo-list"></div>
+        <div className="todo-list">
+            {todos.map((item, index)=>{
+                return <TodoItems key={index} no={item.no} display={item.display} text={item.text}/>
+            })}
+            
+        </div>
     </div>
   )
 }
--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,9 +1,24 @@
 
 import './css/TodoItems.css'
-const TodoItems = () => {
+import tick from './assets/tick.png'
+import not_tick from './assets/not_tick.png'
+import cross from './assets/cross.png'
+
+const TodoItems = ({no, display, text}) => {
   return (
-    <div>TodoItems</div>
+    <div className='todoitems'>
+        <div className="todointem-container">
+            <img src={not_tick} alt="" />
+            <img src={tick} alt="" />
+            <div className="todoitems-text">
+                {text}
+            </div>
+            <img src={cross} alt="" />
+        </div>
+    </div>
   )
 }
 
 export default TodoItems
+
+
